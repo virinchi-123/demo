0d2d21f8003366a12c3548e05a5793f93e932e1f
virinchi <virinchi@VIRINCHI-LAPTOP>
2023-01-09 16:04:13 +0530

    my demo hii

---
+++ Jenkins/src/main/java/demo/Test1.java
@@ -0,0 +1,13 @@
+package demo;
+
+import org.testng.annotations.Test;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Test1 {
+@Test
+public void m1()
+{
+Reporter.log("Hello", true);	
+}
+}
---
+++ Jenkins/src/main/java/demo/Test2.java
@@ -0,0 +1,14 @@
+package demo;
+
+import org.testng.annotations.Test;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Test2 {
+
+	@Test
+	public void m2()
+	{
+		Reporter.log("Hii", true);
+	}
+}
